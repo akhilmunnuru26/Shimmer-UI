73437423d0b9f2169ccac49041cc3c6849801a4d
akhilmunnuru26 <munnuruakhil26@gmail.com>
2026-02-10 15:06:34 +0530

    Practised Infinite Scroll

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,19 +4,45 @@ import Shimmer from './Shimmer';
 
 const Body = () => {
 
-    const [memes, setMemes] = useState(null);
+    const [memes, setMemes] = useState([]); // We will keep initial state as [] instead of null reason is we can't able to append to null anything it will throw error, soo we keep initial state []
+
+    const [showShimmer, setShowShimmer] = useState(false);
     
     useEffect(() => {
         fetchMemes();
+
+        // Adding Scroll Event Listener; Important:- Always Cleanup the Added EventListener
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleaning up the Event Listener
+        return () => window.removeEventListener("scroll", handleScroll)
     }, [])
 
+    const handleScroll = () => {
+
+        // scrollY: How much i have Scrolled
+        // innerHeight: visible Section
+        // document.body.scrollHeight: Total height of the webpage/body
+        // window.scrollY + window.innerHeight >= document.body.scrollHeight if its true that means we have reached bottom of the page.
+
+        if (window.scrollY + window.innerHeight >= document.body.scrollHeight){
+            console.log("Call API Fetch More data");
+            fetchMemes()
+        }
+
+    }
+
+    // Reusing Same function
     const fetchMemes = async () => {
 
         try{
-            const data = await fetch("https://meme-api.com/gimme/25");
+            setShowShimmer(true);
+            const data = await fetch("https://meme-api.com/gimme/10");
             const res = await data.json();
+            setShowShimmer(false);
             console.log("Data", res);
-            setMemes(res.memes);
+            setMemes(memes => [...memes, ...res.memes]);
+            
     
         }catch(e){
             console.log(`${e.message}`)
@@ -27,7 +53,9 @@ const Body = () => {
 
   return (
     <div className='flex flex-row flex-wrap justify-between align-middle px-4'>
-        {!memes ? <Shimmer/> : memes.map((meme,i) => <MemeCard key={i} data={meme}/>)}
+        {/* {!memes ? <Shimmer/> : memes.map((meme,i) => <MemeCard key={i} data={meme}/>)} */}
+         { memes.map((meme,i) => <MemeCard key={i} data={meme}/>)}
+         {showShimmer && <Shimmer/>}
     </div>
   )
 }
--- src/components/MemeCard.js
+++ src/components/MemeCard.js
@@ -4,9 +4,9 @@ const MemeCard = ({data}) => {
     const {url,author, title} = data
   return (
     
-    <div className="w-64 border rounded-lg border-gray-600 shadow-lg p-3 my-4 hover:border-orange-600">
+    <div className="w-60 border rounded-lg border-gray-600 shadow-lg p-3 my-4 hover:border-orange-600">
         <h1 className="font-bold my-1">{title}</h1>
-        <img className="h-60 w-60" src={url} alt={title}/>
+        <img className="h-52 w-52" src={url} alt={title}/>
         <p className="font-sans text-sm my-2 font-light text-indigo-950 italic">{author}</p>
     </div>
   )
